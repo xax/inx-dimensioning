ebb91ef365a6fa2be76e8fc5b0f765bd18fa8399
xax <xa@universe>
2022-06-15 22:15:03 +0200

    Consider stroke width with rects. Documentation.

--- xa_dimensioning.py
+++ xa_dimensioning.py
@@ -9,6 +9,9 @@
 #
 """
 XA Dimensioning extension for Inkscape v1.2+.
+
+Documentation of the inkex API:
+    https://inkscape.gitlab.io/extensions/documentation/index.html
 """
 
 import inkex
@@ -17,65 +20,74 @@ from inkex.localization import inkex_gettext as _
 from math import sqrt
 
 class XADimensioning(inkex.EffectExtension):
-    """XA Measurements extension"""
+    """XA Dimensioning extension."""
 
     ## Interface implementation
 
     def add_arguments(self, pars):
+        """Parse arguments."""
         pars.add_argument("--tab", type=str, default="options")
         pars.add_argument(
             "-H",
             "--hide",
             type=inkex.Boolean,
             default=True,
-            help="Hide dimensioned object after having been annotated",
+            help=_("Hide dimensioned object after having been annotated")
         )
         pars.add_argument(
-            "-p",
+            "-P",
             "--perp",
             type=inkex.Boolean,
             default=False,
-            help="Annotate un-closed paths perpendicularly",
+            help=_("Annotate un-closed paths perpendicularly")
         )
         pars.add_argument(
             "-L",
             "--linecolor",
             type=inkex.Color,
             default=inkex.Color(0xFFA500FF),
-            help="Use document units instead of px",
+            help=_("Color of annotation lines and text")
         )
         pars.add_argument(
             "-u",
             "--useUU",
             type=inkex.Boolean,
             default=True,
-            help="Use document units instead of px",
+            help=_("Use document units (uu) instead of px")
         )
         pars.add_argument(
             "-f",
             "--fontsize",
             type=int,
             default=10,
-            help="Size of length label text in px",
+            help=_("Font size in px or uu")
         )
         pars.add_argument(
             "--xoffset",
             type=float,
             default=50.0,
-            help="x offset of the vertical dimension arrow",
+            help=_("x offset of the vertical dimension arrow")
         )
         pars.add_argument(
             "--yoffset",
             type=float,
             default=50.0,
-            help="y offset of the horizontal dimension arrow",
+            help=_("y offset of the horizontal dimension arrow")
+        )
+        pars.add_argument(
+            "-p",
+            "--precision",
+            type=int,
+            default=2,
+            help=_("Number of significant digits after decimal point")
         )
 
     def effect(self):
+        """Implement the effect extension API."""
         # configuration probably to be made accessable
         self._startOffset = 50
         self._sunit = self.svg.document_unit
-        self._uuperunit = self.svg.unittouu("1" + self._sunit)  # convert to document units
+        self._uuperunit = self.svg.unittouu(f"1{self._sunit}")  # convert to document units
         self._uuperpx = self.svg.unittouu("1px")  # convert px to document units
         #
         if not self.options.useUU:
@@ -104,16 +116,26 @@ class XADimensioning(inkex.EffectExtension):
     ## Private implementation
 
     def _appendNewGroup(self, node, layer):
+        """Append new group.
+
+        Create a new group with default attributes and id refering to node.eid;
+        append it to layer.
+        """
         group = inkex.Group()
         group.set("fill", None)
         group.set("stroke", self.options.linecolor)
         group.set(":xa-dimning", "group")
-        group.label = node.eid + "-xa-dimning"
+        group.label = f"{node.eid}-xa-dimning"
         group.href = node
         layer.append(group)
         return group
 
     def _annotatePathPerpendicularly(self, node, layer):
+        """Indicate and annotate a path (perpendicular mode).
+
+           Uses markers perpendicular to start and end of path, respectively.
+           Dimension is sum of each path segment.
+        """
 
         # csp is a list of of segments, each a list of two+ control data triplets (incoming ctrl, point, outgoing ctrl)
         def csp_seg_reduce_bz(csp_seg, asp=0, bsp=-1):
@@ -153,12 +175,12 @@ class XADimensioning(inkex.EffectExtension):
 
         group = self._appendNewGroup(node, layer)
         line = inkex.PathElement()
-        line.set("d", "M %f %f l %f %f" % (p0[0], p0[1], ay, -ax ))
+        line.set("d", f"M {p0[0]} {p0[1]} l {ay} {-ax}")
         line.set("stroke-width", str(.5 * self._uuperpx))
         #line.set(":xa-debug", "ax=%f ay=%f bx=%f by=%f" % (ax, ay, bx, by))
         group.append(line)
         line = inkex.PathElement()
-        line.set("d", "M %f %f l %f %f" % (p1[0], p1[1], by, -bx ))
+        line.set("d", f"M {p1[0]} {p1[1]} l {by} {-bx}")
         line.set("stroke-width", str(.5 * self._uuperpx))
         #line.set(":xa-debugA", "(%f, %f) (%f, %f) .. (%f, %f) (%f, %f)" % (bzA[0][0],bzA[0][1], bzA[1][0],bzA[1][1], bzA[2][0],bzA[2][1], bzA[3][0],bzA[3][1]))
         #line.set(":xa-debugB", "(%f, %f) (%f, %f) .. (%f, %f) (%f, %f)" % (bzB[0][0],bzB[0][1], bzB[1][0],bzB[1][1], bzB[2][0],bzB[2][1], bzB[3][0],bzB[3][1]))
@@ -166,19 +188,24 @@ class XADimensioning(inkex.EffectExtension):
 
 
         line = inkex.PathElement()
-        line.set("d", "M %f %f L %f %f" % (q0[0], q0[1], q1[0], q1[1]))
+        line.set("d", f"M {q0[0]} {q0[1]} L {q1[0]} {q1[1]}")
         line.set("stroke-width", str(self._uuperpx))
         group.append(line)
 
         textElt = line.add(inkex.TextElement())
         textElt.set(":xa-dimning", "label")
-        textElt.label = node.eid + "-xa-dimning-vert-dim"
-        self._addText(node=textElt, dimVal=stotal, dx=ax*2, dy=ay*2, nodeHref=line, nodeRef=node, dimUnit=self._sunit)
+        textElt.label = f"{node.eid}-xa-dimning-vert-dim"
+        self._addText(node=textElt, dimVal=stotal, dx=ax*1.2, dy=ay*1.2, nodeHref=line, nodeRef=node, dimUnit=self._sunit)
         group.append(textElt)
 
 
 
     def _annotatePath(self, node, layer):
+        """Indicate and annotate a path (grid-orthogonal mode).
+
+           Uses markers along axis direction.
+           Dimension is he largest bbox extent along grid axis.
+        """
         bbox = node.bounding_box()
         dx = 0
         dy = 0
@@ -188,38 +215,38 @@ class XADimensioning(inkex.EffectExtension):
         if bbox.width >= bbox.height:
             line = self._vert_line(bbox.left, [0, 2], bbox)
             line.set("stroke-width", str(.5 * self._uuperpx))
-            line.label = node.eid + "-xa-dimning-horz-l"
+            line.label = f"{node.eid}-xa-dimning-horz-l"
             group.append(line)
             line = self._vert_line(bbox.right, [0, 2], bbox)
             line.set("stroke-width", str(.5 * self._uuperpx))
-            line.label = node.eid + "-xa-dimning-horz-r"
+            line.label = f"{node.eid}-xa-dimning-horz-r"
             group.append(line)
 
             line = self._horz_line(bbox.top, [0, 1], bbox)
             line.set("stroke-width", str(self._uuperpx))
-            line.label = node.eid + "-xa-dimning-horz"
+            line.label = f"{node.eid}-xa-dimning-horz"
             group.append(line)
             textElt = line.add(inkex.TextElement())
             textElt.set(":xa-dimning", "label")
-            textElt.label = node.eid + "-xa-dimning-horz-dim"
+            textElt.label = f"{node.eid}-xa-dimning-horz-dim"
             dy = 2
         else:
             line = self._horz_line(bbox.top, [2, 0], bbox)
             line.set("stroke-width", str(.5 * self._uuperpx))
-            line.label = node.eid + "-xa-dimning-vert-t"
+            line.label = f"{node.eid}-xa-dimning-vert-t"
             group.append(line)
             line = self._horz_line(bbox.bottom, [2, 0], bbox)
             line.set("stroke-width", str(.5 * self._uuperpx))
-            line.label = node.eid + "-xa-dimning-vert-b"
+            line.label = f"{node.eid}-xa-dimning-vert-b"
             group.append(line)
 
             line = self._vert_line(bbox.right, [-1, 0], bbox)
             line.set("stroke-width", str(self._uuperpx))
-            line.label = node.eid + "-xa-dimning-vert"
+            line.label = f"{node.eid}-xa-dimning-vert"
             group.append(line)
             textElt = line.add(inkex.TextElement())
             textElt.set(":xa-dimning", "label")
-            textElt.label = node.eid + "-xa-dimning-vert-dim"
+            textElt.label = f"{node.eid}-xa-dimning-vert-dim"
             dx = 2
 
         self._addText(node=textElt, dimVal=bbox.height, dx=dx, dy=dy, nodeHref=line, nodeRef=node, dimUnit=self._sunit)
@@ -229,72 +256,79 @@ class XADimensioning(inkex.EffectExtension):
             node.style["display"] = "none"
 
     def _annotateRect(self, node, layer):
+        """Indicate and annotate a rectangle.
+
+           Uses markers along grid axes.
+           Dimension is measured along grid axes, accordingly.
+        """
         bbox = node.bounding_box()
+        stroke = inkex.units.convert_unit(node.get("stroke-width", "1px"), "px") * self._uuperpx
 
         group = self._appendNewGroup(node, layer)
 
         lineH = self._horz_line(bbox.top, [0, 1], bbox)
         lineH.set("stroke-width", str(self._uuperpx))
-        lineH.label = node.eid + "-xa-dimning-horz"
+        lineH.label = f"{node.eid}-xa-dimning-horz"
         group.append(lineH)
 
         line = self._vert_line(bbox.left, [0, 2], bbox)
         line.set("stroke-width", str(.5 * self._uuperpx))
-        line.label = node.eid + "-xa-dimning-horz-l"
+        line.label = f"{node.eid}-xa-dimning-horz-l"
         group.append(line)
         line = self._vert_line(bbox.right, [0, 2], bbox)
         line.set("stroke-width", str(.5 * self._uuperpx))
-        line.label = node.eid + "-xa-dimning-horz-r"
+        line.label = f"{node.eid}-xa-dimning-horz-r"
         group.append(line)
 
         lineV = self._vert_line(bbox.right, [-1, 0], bbox)
         lineV.set("stroke-width", str(self._uuperpx))
-        lineV.label = node.eid + "-xa-dimning-vert"
+        lineV.label = f"{node.eid}-xa-dimning-vert"
         group.append(lineV)
 
         line = self._horz_line(bbox.top, [2, 0], bbox)
         line.set("stroke-width", str(.5 * self._uuperpx))
-        line.label = node.eid + "-xa-dimning-vert-t"
+        line.label = f"{node.eid}-xa-dimning-vert-t"
         group.append(line)
         line = self._horz_line(bbox.bottom, [2, 0], bbox)
         line.set("stroke-width", str(.5 * self._uuperpx))
-        line.label = node.eid + "-xa-dimning-vert-b"
+        line.label = f"{node.eid}-xa-dimning-vert-b"
         group.append(line)
 
         textElt = lineH.add(inkex.TextElement())
         textElt.set(":xa-dimning", "label")
-        textElt.label = node.eid + "-xa-dimning-horz-dim"
-        self._addText(node=textElt, dimVal=bbox.width, dy=2, nodeHref=lineH, nodeRef=node, dimUnit=self._sunit)
+        textElt.label = f"{node.eid}-xa-dimning-horz-dim"
+        self._addText(node=textElt, dimVal=bbox.width+stroke, dy=2, nodeHref=lineH, nodeRef=node, dimUnit=self._sunit)
         group.append(textElt)
 
         textElt = lineV.add(inkex.TextElement())
         textElt.set(":xa-dimning", "label")
-        textElt.label = node.eid + "-xa-dimning-vert-dim"
-        self._addText(node=textElt, dimVal=bbox.height, dx=2, nodeHref=lineV, nodeRef=node, dimUnit=self._sunit)
+        textElt.label = f"{node.eid}-xa-dimning-vert-dim"
+        self._addText(node=textElt, dimVal=bbox.height+stroke, dx=2, nodeHref=lineV, nodeRef=node, dimUnit=self._sunit)
         group.append(textElt)
 
         if self.options.hide:
             node.style["display"] = "none"
 
     def _horz_line(self, y, xlat, bbox):
-        """Create a horzontal line"""
+        """Create a horzontal line."""
         line = inkex.PathElement()
         x1 = bbox.left
         x2 = bbox.right + xlat[0] * self.options.xoffset
         y1 = y - xlat[1] * self.options.yoffset
-        line.set("d", "M %f %f H %f" % (x1, y1, x2))
+        line.set("d", f"M {x1} {y1} H {x2}")
         return line
 
     def _vert_line(self, x, xlat, bbox):
-        """Create a vertical line"""
+        """Create a vertical line."""
         line = inkex.PathElement()
         x = x - xlat[0] * self.options.xoffset
         y1 = bbox.top - xlat[1] * self.options.yoffset
         y2 = bbox.bottom
-        line.set("d", "M %f %f V %f" % (x, y1, y2))
+        line.set("d", f"M {x} {y1} V {y2}")
         return line
 
     def _addText(self, node, dimVal, *, nodeHref, nodeRef=None, dx=0, dy=0, dimUnit=None):
+        """Create text along path."""
         style = {
             "text-align": "center",
             "vertical-align": "bottom",
@@ -304,14 +338,14 @@ class XADimensioning(inkex.EffectExtension):
             "stroke": "none",
             "font-weight": "normal",
             "font-style": "normal",
-            "fill": self.options.linecolor,
+            "fill": str(self.options.linecolor),
         }
         newNode = inkex.TextPath()
         newNode.set(":xa-dimning", "text")
         if nodeRef is not None:
             newNode.set(":xa-dimning-ref", nodeRef.eid)
-            newNode.label = nodeRef.eid + "-dim"
-        newNode.set("startOffset", str(self._startOffset) + "%")
+            newNode.label = f"{nodeRef.eid}-dim"
+        newNode.set("startOffset", f"{self._startOffset}%")
         if dy != 0:
             newNode.set("dy", str(-dy))
             newNode.set("dx", str(-nodeHref.bounding_box().width/2*self._uuperpx))
@@ -320,8 +354,8 @@ class XADimensioning(inkex.EffectExtension):
             newNode.set("dx", str(-nodeHref.bounding_box().height/2*self._uuperpx))
         newNode.style = style
         newNode.href = nodeHref
-        #newNode.text = self.svg.add_unit(round(dimVal, 2), dimUnit)
-        newNode.text = inkex.units.render_unit(round(dimVal, 2), dimUnit)
+        #newNode.text = self.svg.add_unit(round(dimVal, self.options.precision), dimUnit)
+        newNode.text = inkex.units.render_unit(round(dimVal, self.options.precision), dimUnit)
         node.add(newNode)
 
 
